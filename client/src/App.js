51c249dca04b5535e42d550e9c69f24ab6fc9681
Alex <alexcouts18@gmail.com>
2020-07-22 18:17:15 -0500

    added commenting on UX decisions

--- client/src/App.js
+++ client/src/App.js
@@ -36,8 +36,8 @@ function App() {
           </div>
       }  
     </div>
-  );
-}
+  )
+};
 
 export default App;
 
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -8,6 +8,10 @@ const SignUpForm = () => {
     // * AJAX req to add new user to the database 
 
     // ! determine UX after sign up for different user types
+        // * operator: add truck to database form 
+            // * handled on the back end with T/F value returned 
+        
+        // * diner: list of trucks 
 
 
     return(
