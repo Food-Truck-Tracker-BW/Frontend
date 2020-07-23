f712d14ee5296e495135c8607af3158999de3329
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-23 16:21:52 -0700

    first

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,7 +1,41 @@
 // * dependencies:
 import React from 'react'
+import axios from 'axios'
+import { Form, Field } from 'formik'
+import * as yup from "yup"
 
 
+// const handleSubmit = e => {
+//     e.preventDefault();
+//     // console.log(formState);
+//     axios.post("https://reqres.in/api/users", formState)
+//     .then(res => props.(res.data))
+//     .catch(err => console.log(err));
+// }
+
+// const LoginValidation = yup.object().shape({
+//     email: yup
+//       .string()
+//       .email()
+//       .required(),
+//     password: yup
+//       .string()
+//       .min(8)
+//       .max(16)
+//       .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$")
+//       .required(),
+//   })
+// export default withFormik({
+//     // Handles our submission
+//     handleSubmit: (values, { setSubmitting }) => {
+//     // This is where you could send the submitted values to the backend
+//     console.log("Submitted Email:", values.email)
+//     console.log("Submitted Password:", values.password)
+//     // Simulates the delay of a real request
+//     setTimeout(() => setSubmitting(false), 3 * 1000)
+//   },
+//   validationSchema: LoginValidation,
+// })(LoginWrapper)
 
 const LoginPage = () => {
 
@@ -10,8 +44,14 @@ const LoginPage = () => {
 
     return (
         <div>
-            Login Page! 
+            <h1>Login Page!</h1> 
+            {/* <Form>
+                <Field type="text" name="email" placeholder="email" />
+                <Field type="text" name="password" placeholder="password" />
+                <button type="submit">Submit</button>
+            </Form> */}
         </div>
+        
     )
 };
 
