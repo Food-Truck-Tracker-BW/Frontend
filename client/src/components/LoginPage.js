dafe368f51aaabe292de5f1c73cb70b74f1120e4
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-23 16:54:15 -0700

    first

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,67 @@
 // * dependencies:
 import React from 'react'
 import axios from 'axios'
-import { Form, Field } from 'formik'
-import * as yup from "yup"
+import { Form, Field,formik } from 'formik'
+// import * as yup from "yup"
 
 
+return( <div className="signup-container ">
+        <Formik
+        initialValues={{
+            name: '',
+            email: '',
+            password: '',
+            position: '',
+            
+        validationSchema={Yup.object({
+         name: Yup.string()
+         .min(5, 'Must be at least 5 characters!')
+         .max(10, 'Must be at least 10 chracters!')
+         .required('Required'),
+         email: Yup.string()
+         .email('Invalid email address!')
+         .required('Required'),
+         password: Yup.string()
+         .matches(/(?=.*[a-z])/, 'one lowercase required!')
+         .matches(/(?=.*[A-Z])/, 'one uppercase required!')
+         .matches(/(?=.*[0-9])/, 'one number required!')
+         .min(6, 'Must be at least 6 characters')
+         .required('Required'),
+         passwordConfirm: Yup.string()
+         .oneOf([Yup.ref('password')], 'Password must be the same!')
+         .required('Required'),
+         position: Yup.string()
+         .oneOf(['Diner', 'Food Truck Operator'],)
+         .required('Required'),
+        
+        })}
+        onSubmit={(values, {setSubmitting, resetForm}) => {
+            setTimeout(()=> {
+                alert(JSON.stringify(values, null, 2))
+                resetForm()
+                setSubmitting(false)
+            }, 1000)
+        }}
+        >
+            {props => (
+                <Form>
+                    <h1>Sign up</h1>
+                    <CustomTextInput label="Name" name="name" type="text   " placeholder="Enter username" />
+                    <CustomTextInput label="Email" name="email" type="email   " placeholder="Enter Email" />
+                    
+                    <CustomSelect  label="Positions" name="postions">
+                        <option value="">Select a position</option>
+                        <option value="Food Diner">Food Diner</option>
+                        <option value="Food Truck Operator">Food Truck Operator</option>
+                    </CustomSelect>
+                     
+                    <button onSubmit={onsubmit}></button>
+                </Form>
+            )}
+        </Formik>
+    </div>)
+}
+export default LogIn
 // const handleSubmit = e => {
 //     e.preventDefault();
 //     // console.log(formState);
@@ -42,17 +99,17 @@ const LoginPage = () => {
     // ! axios login logic 
 
 
-    return (
-        <div>
-            <h1>Login Page!</h1> 
-            {/* <Form>
-                <Field type="text" name="email" placeholder="email" />
-                <Field type="text" name="password" placeholder="password" />
-                <button type="submit">Submit</button>
-            </Form> */}
-        </div>
+//     return (
+//         <div>
+//             <h1>Login Page!</h1> 
+//             {/* <Form>
+//                 <Field type="text" name="email" placeholder="email" />
+//                 <Field type="text" name="password" placeholder="password" />
+//                 <button type="submit">Submit</button>
+//             </Form> */}
+//         </div>
         
-    )
-};
+//     )
+// };
 
-export default LoginPage
+// export default LoginPage
