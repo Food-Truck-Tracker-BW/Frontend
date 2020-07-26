b3246b717def4103be47f556ad0c22bf39521545
Alex <alexcouts18@gmail.com>
2020-07-25 23:50:22 -0500

    mocked single view page

---
+++ client/src/components/SingleTruckView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SingleTruck = (props) => {
+
+
+
+    return(
+        <div className='single-truck-container'>
+            Am I the truck you expected?
+        </div>
+    )
+}
+
+export default SingleTruck;
